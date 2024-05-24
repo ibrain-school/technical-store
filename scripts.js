e585d3ffea8dcb224ba1be9081a26293cab0eed4
israpilovaevgeniia <israpilovaevgeniia@gmail.com>
2024-05-24 17:39:26 -0500

    Create popular categories

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener ('DOMContentLoaded', function(){
+
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        slidesPerView: 6,
+        initialSlide: 1, 
+        loopedSlides: 6,
+        navigation: {
+            prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            clickable: false,
+        },  
+
+})
+
+});
