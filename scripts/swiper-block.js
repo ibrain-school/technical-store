a9705f08017eb1b8cfd623b6c14ae559726e495a
israpilovaevgeniia <israpilovaevgeniia@gmail.com>
2024-06-02 12:19:02 -0500

    created section about block

--- scripts/swiper-block.js
+++ scripts/swiper-block.js
@@ -2,7 +2,7 @@ document.addEventListener ('DOMContentLoaded', function(){
     const swiper = new Swiper('.block__swiper', {
         loop: true,
         slidesPerView: 4,
-        initialSlide: 1, 
+        initialSlide: 0, 
         loopedSlides: 4,
         navigation: {
             prevEl: '.swiper-button-prev',
