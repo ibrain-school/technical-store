e24dfe376ac04dd3feeab65307e0bb6dc4949022
AlexMalkov01 <Alex228369@yandex.ru>
2024-06-05 19:15:44 +0300

    1

--- scripts/yandex-map.js
+++ scripts/yandex-map.js
@@ -26,6 +26,53 @@ function init() {
             iconImageOffset: [-50, -70]
         });
 
+        placemark.events.add("click",(e)=>{
+            const div = document.querySelector(".card-shop")
+            div.innerHTML = `
+            <h2 class="card-shop_descripton">Магазин в ТЦ «Корона-Сити», 1 этаж</h2>
+            <div class="card-shop_wrapper-content">
+            <img src="/assets/img/korona-shopping-center.png" alt="shopping-center_img"> 
+            <div class="card-shop_nav">
+
+                <div class="card-shop_nav--wrap">
+
+                    <div class="card-shop_nav--title">
+                        <img src="/assets/icons/marker-yndex-card.svg" alt="marker"> 
+                        <h3>Адрес:</h3>
+                    </div>
+                    <span>
+                    ул. Денисовская, 8
+                    </span>
+
+                </div>
+
+                <div class="card-shop_nav--wrap">
+
+                    <div class="card-shop_nav--title">
+                        <img src="/assets/icons/yandex-map-tel.svg" alt="tel"> 
+                        <h3>Справочный центр:</h3>
+                    </div>
+                    <div class="card-shop_nav-contact">
+                        <span>+375 29 8-057-998 МТС</span>
+                        <span>+375 29 3-899-741 А1</span>
+                        <a href="tel:+375 17 388 61 41">+375 17 388 61 41</a>
+                    </div>
+                </div>
+
+
+                <div class="card-shop_nav--wrap">
+
+                <div class="card-shop_nav--title">
+                    <img src="/assets/icons/watch-yandex-map.svg" alt="watch"> 
+                    <h3>График работы:</h3>
+                </div>
+                <span>
+                пн-вс: 10:00-21:00
+                </span>
+            </div>
+            `
+            div.style.cssText = `margin-bottom: 100px;`
+        })
         myMap.geoObjects.add(placemark);
         geoObjects.push(placemark);
     });
