51aa40b5642c4ad579bcdb81ed479060fe249574
Sergey86fr <sergsfree@gmail.com>
2024-06-14 16:59:19 +0500

    adaptive about_us task-37

---
+++ scripts/about-us.js
@@ -0,0 +1,7 @@
+function togglemenu() {
+    document.getElementsByClassName("about-company__list")[0] .classList.toggle("open");
+}
+
+function togglemenu1() {
+    document.getElementsByClassName("about-buyers__list")[0] .classList.toggle("open1");
+}
